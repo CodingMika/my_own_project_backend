6609c97d1f02fb8a1502bf4e606b2aa3584e328f
CodingMika <feline.snarl@gmail.com>
2022-09-22 16:36:27 +0200

    validation data

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -6,6 +6,14 @@ router.post("/registration", async (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
+  const errors = handleValidation(name, email, password);
+
+  if (errors.length > 0) {
+    console.log(errors);
+    res.status(400).json({ error: errors });
+    return;
+  }
+
   const findUser = await User.findOne({
     email: email,
   }).exec();
@@ -26,4 +34,21 @@ router.post("/registration", async (req, res) => {
       res.status(400).json(["unable to save to database;" + err]);
     });
 });
+
+function handleValidation(name, email, password) {
+  let errors = [];
+  if (name.length < 1 || name.length > 10) {
+    errors.push("Your name should be min 1 letter and max 10 letters.");
+  }
+  if (email == "") {
+    errors.push("Please, provide your email.");
+  }
+  if (password.length !== 60) {
+    errors.push(
+      "Please enter a password that contains from 6 to 12 characters."
+    );
+  }
+  return errors;
+}
+
 module.exports.userRoutes = router;
