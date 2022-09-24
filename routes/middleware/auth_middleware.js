9d467825ede02c481e62f9a4c9f5ceb38f05bbf7
CodingMika <feline.snarl@gmail.com>
2022-09-24 14:24:29 +0200

    make log in

---
+++ routes/middleware/auth_middleware.js
@@ -0,0 +1,4 @@
+module.exports.isAutheticated = (req, res, next) => {
+  if (req.session.user) return next();
+  res.status(401).json();
+};
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,12 +2,35 @@ const express = require("express");
 const User = require("../database/models/user");
 const router = express.Router();
 
+router.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const errors = handleValidationLogin(email, password);
+  if (errors.length > 0) {
+    console.log(errors);
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const findUser = await User.findOne({
+    email: email,
+    password: password,
+  }).exec();
+  if (findUser == null) {
+    res.status(400).json({ error: "Your email or password is invalid." });
+    return;
+  }
+  console.log([req.body, findUser]);
+  res.json(findUser);
+});
+
 router.post("/registration", async (req, res) => {
+  console.log(req.body);
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-  const errors = handleValidation(name, email, password);
 
+  const errors = handleValidationRegistration(name, email, password);
   if (errors.length > 0) {
     console.log(errors);
     res.status(400).json({ error: errors });
@@ -35,18 +58,21 @@ router.post("/registration", async (req, res) => {
     });
 });
 
-function handleValidation(name, email, password) {
+function handleValidationLogin(email, password) {
   let errors = [];
-  if (name.length < 1 || name.length > 10) {
-    errors.push("Your name should be min 1 letter and max 10 letters.");
-  }
   if (email == "") {
     errors.push("Please, provide your email.");
   }
-  if (password.length !== 60) {
-    errors.push(
-      "Please enter a password that contains from 6 to 12 characters."
-    );
+  if (password.length !== 64) {
+    errors.push("The password is invalid.");
+  }
+  return errors;
+}
+
+function handleValidationRegistration(name, email, password) {
+  let errors = handleValidationLogin(email, password);
+  if (name.length < 1 || name.length > 10) {
+    errors.push("Your name should be min 1 letter and max 10 letters.");
   }
   return errors;
 }
